7d44d520b50b43a1efcf59970266779544c15ab7
José Eduardo <75598729+Ze-Edu@users.noreply.github.com>
2023-03-26 12:58:53 -0300

    LogicasDoCheckBoxJavafxScenebuilder

--- JavaAplication/src/TelaSintomasController.java
+++ JavaAplication/src/TelaSintomasController.java
@@ -1,7 +1,13 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 
 public class TelaSintomasController {
 
@@ -9,7 +15,7 @@ public class TelaSintomasController {
     private Button btnAva;
 
     @FXML
-    private CheckBox check1;
+    public CheckBox check1;
 
     @FXML
     private CheckBox check2;
@@ -36,8 +42,37 @@ public class TelaSintomasController {
     private CheckBox check9;
 
     @FXML
+    private void Action() {}
+
+    @FXML
     void btnSintomas(ActionEvent event) {
-        
+    
+        if(check1.isSelected() && check5.isSelected() && check6.isSelected() && check7.isSelected() == true){
+        FXMLLoader fx = new FXMLLoader(LoginController.class.getResource("Telafinal2.fxml"));
+         try {
+             Scene s = new Scene(fx.load());
+             Stage st = new Stage();
+             st.setTitle("AplicaçãoJava-Covid19");
+             st.setScene(s);
+             st.show();
+
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+        }else{
+        FXMLLoader fx = new FXMLLoader(LoginController.class.getResource("Telafinal1.fxml"));
+         try {
+             Scene s = new Scene(fx.load());
+             Stage st = new Stage();
+             st.setTitle("AplicaçãoJava-Covid19");
+             st.setScene(s);
+             st.show();
+
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+        }
+
     }
 
 }
